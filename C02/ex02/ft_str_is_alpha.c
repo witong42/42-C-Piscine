b1c26b5cfa23c847e907b670c572dcf1f7e2cf68
William <william-mbp@williams-mbp.home>
2024-03-17 22:15:20 +0100

    Update

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,18 @@
+int	ft_str_is_alpha(char *str);
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z')
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
