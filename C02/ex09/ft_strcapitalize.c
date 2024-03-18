1870fcbb9f73790aa52a4d9ee31e140b9cad24ac
William <william-mbp@williams-mbp.home>
2024-03-18 21:57:24 +0100

    Update

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -23,7 +23,7 @@ char	*ft_strcapitalize(char *str)
 		}
 	while (str[i])
 	{
-		if (((str[i - 1] < '0' || (str[i - 1] > '9' && str[i - 1] < 'A') || (str[i - 1] > 'Z' && str[i - 1] < 'a') || str[i - 1] > 'z'))
+			if (!((str[i - 1] >= '0' && str[i - 1] <= '9') || (str[i - 1] >= 'A' && str[i - 1] <= 'Z') || (str[i - 1] >= 'a' && str[i - 1] <= 'z')))
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] -= 32;
---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	len;
+
+	i = 0;
+	len = 0;
+	while (src[len])
+	{
+		len++;
+	}
+	if (size < 1)
+	{
+		return (len);
+	}
+	while (src[i] && i < size -1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (len);
+}
+
+int	main(void)
+{
+	char	src[] = "I love cheese and burgers";
+	char	dest[12];
+	unsigned int	len;
+	
+	len = ft_strlcpy(dest, src, sizeof(dest));
+	printf("Copied string: %s\n", dest);
+	printf("Length of the source string: %d\n", len);
+	return (0);
+}
