3c3f0b55bb03f7931793f1cc1d188b88a0d3b8b4
William <william-mbp@williams-mbp.home>
2024-03-19 23:08:40 +0100

    Update

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -10,9 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
+//#include <stdio.h>
+//
 char	*ft_strstr(char *str, char *to_find)
 {
+	int	i;
+	int	j;
 
+	i = 0;
+	if (to_find[0] == '\0')
+	{
+		return(str);
+	}
+	while (str[i])
+	{
+		j = 0;
+		while (str[i + j] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+			{
+				return (&str[i]);
+			}
+			j++;
+		}	
+		i++;
+	}
+	return (0);
 }
+/*
+int	main(void)
+{
+	char	*haystack = "There is a needle in the haystack";
+	char	*needle = "needle";
+	char	*result;
+	
+	result = ft_strstr(haystack, needle);
+	printf("%s\n", result);
+	return (0);
+}
+*/
