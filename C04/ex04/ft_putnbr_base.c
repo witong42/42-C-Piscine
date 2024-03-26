26218c8f5d8003eeb429004821553b354f749bd4
William <william-mbp@williams-mbp.home>
2024-03-26 21:00:31 +0100

    Update

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -22,6 +22,9 @@ int	is_base_invalid(char *base)
 	i = 0;
 	while (base[i])
 	{
+		if (base[i] == '+' || base[i] == '-'
+				|| base [i] < 33 || base[i] > 126)
+			return (1);
 		j = i + 1;
 		while (base[j])
 		{
@@ -29,10 +32,6 @@ int	is_base_invalid(char *base)
 				return (1);
 			j++;
 		}
-		if (base[i] == '+' || base[i] == '-')
-			return (1);
-		else if (base[i] < 32 || base[i] > 126)
-			return (1);
 		i++;
 	}
 	return (0);
@@ -63,7 +62,7 @@ void	ft_putnbr_base(int nbr, char *base)
 		}
 	}
 }
-/*
+
 int	main(void)
 {
 	ft_putnbr_base(42, "01");
@@ -82,6 +81,19 @@ int	main(void)
 	write(1, "\n", 1);
 	ft_putnbr_base(42, "poneyvif");
 	write(1, "\n", 1);
+	write(1, "42:", 3);
+	ft_putnbr_base(42, "0123456789");
+	write(1, "\n2a:", 4);
+	ft_putnbr_base(42, "0123456789abcdef");
+	write(1, "\n-2a:", 5);
+	ft_putnbr_base(-42, "0123456789abcdef");
+	write(1, "\n:", 2);
+	ft_putnbr_base(42, "");
+	write(1, "\n:", 2);
+	ft_putnbr_base(42, "0");
+	write(1, "\n:", 2);
+	ft_putnbr_base(42, "+-0123456789abcdef");
+	write(1, "\n:", 2);
+	ft_putnbr_base(42, "\t0123456789abcdef");
 	return (0);
 }
-*/
