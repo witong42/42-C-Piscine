e70caaff63629453697bff476c3c653dfe0cb8aa
William <william-mbp@williams-mbp.home>
2024-04-01 23:26:31 +0200

    Update

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -18,6 +18,8 @@ int	is_sep(char *charset, char c)
 	int	i;
 
 	i = 0;
+	if (c  == '\0')
+		return (1);
 	while (charset[i])
 	{
 		if (charset[i] == c)
@@ -29,22 +31,18 @@ int	is_sep(char *charset, char c)
 
 int	count_words(char *str, char *charset)
 {
-	int	i;
-	int	words;
+	int	c_count;
+	int	w_count;
 
-	i = 0;
-	words = 0;
-	while (str[i] && charset[i])
+	c_count = 0;
+	w_count = 0;
+	while (str[c_count])
 	{
-		while (is_sep(charset, str[i]) == 1)
-			i++;
-		if (!str[i])
-			break ;
-		while (is_sep(charset, str[i]) == 0)
-			i++;
-		words++;
+		if (is_sep(charset, str[c_count]) == 0 && is_sep(charset, str[c_count + 1]) == 1)
+			w_count++;
+		c_count++;
 	}
-	return (words);
+	return (w_count);
 }
 
 char	*ft_strdup_words(char *str, int total_len, int word_len)
@@ -75,16 +73,17 @@ void	split(char **array, char *str, char *charset)
 	total_len = 0;
 	while (str[total_len])
 	{
-		word_len = 0;
-		while (is_sep(charset, str[total_len]) == 1)
+		if (is_sep(charset, str[total_len]) == 1)
 			total_len++;
-		if (!str[total_len])
-			break ;
-		while (is_sep(charset, str[total_len + word_len]) == 0)
-			word_len++;
-		array[i] = ft_strdup_words(str, total_len, word_len);
-		total_len += word_len;
-		i++;
+		else
+		{
+			word_len = 0;
+			while (is_sep(charset, str[total_len + word_len]) == 0)
+				word_len++;
+			array[i] = ft_strdup_words(str, total_len, word_len);
+			total_len += word_len;
+			i++;
+		}
 	}
 }
 
@@ -108,7 +107,7 @@ int	main(int ac, char **av)
 	char	**display;
 	char	**s;
 
-	s = ft_split("Success", " ");
+	s = ft_split("I love cheeseburgers and fries and diet coke", " ");
 	i = 0;
 	while (s[i])
 	{
