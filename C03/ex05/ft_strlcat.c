c1de47a1f13afe438ace441ff4d86ccb0b590051
williamtng <williamtong1207@gmail.com>
2024-04-16 19:32:53 +0200

    Correct strlcat

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: witong <witong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:36:40 by witong            #+#    #+#             */
-/*   Updated: 2024/03/20 15:42:31 by witong           ###   ########.fr       */
+/*   Updated: 2024/04/16 19:16:49 by witong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-//#include <bsd/string.h>
-//
+#include <stdio.h>
+#include <string.h>
+
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	len_dest;
@@ -22,7 +22,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	len_dest = 0;
 	len_src = 0;
 	i = 0;
-	while (dest[len_dest])
+	while (dest[len_dest] && len_dest < size)
 	{
 		len_dest++;
 	}
@@ -30,27 +30,41 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	{
 		len_src++;
 	}
-	while (src[i] && (len_dest + i < size -1))
+	while (src[i] && (len_dest + i + 1) < size)
 	{
 		dest[len_dest + i] = src[i];
 		i++;
 	}
-	dest[len_dest + i] = '\0';
-	if (len_dest > size)
-		return (len_src + size);
-	else
-		return (len_dest + len_src);
+	if (len_dest + i < size)
+		dest[len_dest + i] = '\0';
+	return (len_dest + len_src);
 }
-/*
+
 int	main(void)
 {
-	char src1[10] = "William";
-	char dest1[10] = "4242";
-	char src[10] = "William";
-	char dest[10] = "4242";
+	char s1a[10] = "Test1";
+	char s2a[] = "OK";
+	char s1b[10] = "Test1";
+	char s2b[] = "OK";
+	char s3a[10] = "Same";
+	char s4a[] = "Size";
+	char s3b[10] = "Same";
+	char s4b[] = "Size";
+	char s5a[20] = "Shorter";
+	char s6a[] = "ThanMyself";
+	char s5b[20] = "Shorter";
+	char s6b[] = "ThanMyself";
+	char s7a[20] = "Shorter";
+	char s8a[] = "ThanMyself";
+	char s7b[20] = "Shorter";
+	char s8b[] = "ThanMyself";
 
-	printf("strlcat: %zu\n", strlcat(dest1, src1, 8));
-	printf("ft_strlcat: %u", ft_strlcat(dest, src, 8));
-	return (0);
+	printf("%s\n", ft_strlcat(s1a, s2a, 6) == strlcat(s1b, s2b, 6) ? "Success" : "Fail");
+	printf("%s\n", strcmp(s1a, s1b) == 0 && strcmp(s2a, s2b) == 0 ? "Success" : "Fail");
+	printf("%s\n", ft_strlcat(s3a, s4a, 10) == strlcat(s3b, s4b, 10) ? "Success" : "Fail");
+	printf("%s\n", strcmp(s3a, s3b) == 0 && strcmp(s4a, s4b) == 0 ? "Success" : "Fail");
+	printf("%s\n", ft_strlcat(s5a, s6a, 4) == strlcat(s5b, s6b, 4) ? "Success" : "Fail");
+	printf("%s\n", strcmp(s5a, s5b) == 0 && strcmp(s6a, s6b) == 0 ? "Success" : "Fail");
+	printf("%s\n", ft_strlcat(s7a, s8a, 0) == strlcat(s7b, s8b, 0) ? "Success" : "Fail");
+	printf("%s\n", strcmp(s7a, s7b) == 0 && strcmp(s8a, s8b) == 0 ? "Success" : "Fail");
 }
-*/
